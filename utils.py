687e1f0c8398d8a0bf850283f3cedaa9be4a8fe6
ceconelo <thiceconelo@gmail.com>
2022-04-05 12:37:53 -0300

    Project upload (main, utils, requirements and readme).

---
+++ utils.py
@@ -0,0 +1,21 @@
+import unicodedata
+import re
+
+
+def slug(value, allow_unicode=False):
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
+
+
+def get_headers():
+    return {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                  'application/signed-exchange;v=b3;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/99.0.4844.84 Safari/537.3 '
+    }
